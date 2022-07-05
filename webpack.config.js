567b29983ff817c949c6e4a8ab47e8ddfd2709bf
qp0kycHuk <serega990112@mail.ru>
2022-07-05 19:55:37 +0300

    add auth page

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const htmlWebpackPluginDefaults = {
   inject: 'head'
 }
 
-const pages = ['index.html', 'ui.html']
+const pages = ['index.html', 'ui.html', 'auth.html']
 const dialogs = ['dialog-large.html', 'dialog-middle.html', 'dialog-small.html']
 
 module.exports = {
